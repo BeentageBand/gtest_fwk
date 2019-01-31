a6df6969847305bb117632e93e7838d874c67943
Roberto Alanis <roalanis@amazon.com>
2019-01-30 19:56:24 -0800

    fix last googletest update, now using googlemock

--- gtest_fwk_make.mk
+++ gtest_fwk_make.mk
@@ -1,22 +1,25 @@
 $(_flavor_)_PROJ_INC+=$($(_flavor_)_GTEST_PATH)/googlemock/include
 $(_flavor_)_PROJ_INC+=$($(_flavor_)_GTEST_PATH)/googletest/include
 
-$(_flavor_)_clean+=$($(_flavor_)_GTEST_PATH)/googletest/make/*.[oa]
-$(_flavor_)_clean+=$($(_flavor_)_GTEST_PATH)/googlemock/make/*.[oa]
+$(_flavor_)_clean+=\
+$($(_flavor_)_GTEST_PATH)/googlemock/make/*.[oa] \
+$($(_flavor_)_GTEST_PATH)/googletest/make/*.[oa] \
 
 $(_feat_)_lib=\
 	gmock \
 	gmock_main \
+	gtest
 
 define $(_flavor_)_GTEST_MAKE
-$($(_flavor_)_LIB_DIR)/lib$(1).a : $($(_flavor_)_GTEST_PATH)/googlemock/make/$(1).a $($(_flavor_)_LIB_DIR)
+$($(_flavor_)_LIB_DIR)/lib$(1).a : $($(_flavor_)_GTEST_PATH)/googlemock/make/lib$(1).a
 	-cp -Pf $$< $$@;
 
-$($(_flavor_)_GTEST_PATH)/googlemock/make/$(1).a : $($(_flavor_)_GTEST_PATH)/googlemock/make/Makefile $($(_flavor_)_GTEST_PATH)/googletest/make/Makefile
-	$(MAKE) all -C $($(_flavor_)_GTEST_PATH)/googletest/make 
-	$(MAKE) all -C $($(_flavor_)_GTEST_PATH)/googlemock/make 
+$($(_flavor_)_GTEST_PATH)/googlemock/make/lib$(1).a : $($(_flavor_)_GTEST_PATH)/googlemock/make 
+	-make lib$(1).a -C $($(_flavor_)_GTEST_PATH)/googlemock/make  CXX=$(CPP)
 
 endef
+
+-include $(PROJ_MAK_DIR)/$($(_flavor_)_build)_makefile.mk
 
 ifndef $(_flavor_)_GTEST_OVERRIDE
 
